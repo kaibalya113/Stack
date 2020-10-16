5bf3dd803dbf066c42219514570ac323170429a8
kaibalya123 <kaibalyamohanty0@gmail.com>
2020-10-16 23:09:11 +0530

    stack implementation

---
+++ src/StackMain.java
@@ -0,0 +1,50 @@
+
+class Stack{
+	// solving using array
+	int stack[] = new int[5];
+	int top = 0;
+	public void push(int data) {
+		
+		stack[top] = data;
+		top++;
+	}
+	void display() {
+		for(int no : stack) {
+			System.out.println(no+" ");
+		}
+	}
+	public int pop() {
+		int data;
+		top--;
+		data = stack[top];
+		stack[top] = 0;
+		
+		return data;
+	}
+	int peek() {
+		int data;
+		data = stack[top-1];
+		return data;
+	}
+	int getSize() {
+		return top;
+	}
+	boolean isEmpty() {
+		return top <= 0;
+	}
+}
+public class StackMain {
+	public static void main(String[] args) {
+		Stack stack = new Stack();
+		stack.push(1);
+		stack.push(2);
+		stack.push(3);
+		stack.push(4);
+		
+		System.out.println("deleted value is: "+stack.pop());
+		System.out.println("peek: "+stack.peek());
+		System.out.println("size of the stack: "+stack.getSize());
+		System.out.println("Is stack Empty: "+stack.isEmpty());
+		stack.display();
+	}
+}
